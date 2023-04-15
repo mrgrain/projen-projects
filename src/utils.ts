8ee140d0337a13a9a73c1ad262c996dfffb82ce6
Momo Kornher <mail@moritzkornher.de>
2023-04-15 20:52:03 +0100

    feat: export utils (#4)
    These are super useful helpers that I might use in other projects.

--- src/utils.ts
+++ src/utils.ts
@@ -21,28 +21,46 @@ export function noEmpties(list: any[]): any[] {
  * User options take precedence.
  */
 export function deepDefaults<T>(options: Partial<T> = {} as any, defaults: Partial<T> = {} as any): T {
-  return mergeDeep(defaults, options);
+  return deepMerge(defaults, options);
 }
 
-function mergeDeep(...data: any[]): any {
-  return data.reduce((result, current) => {
+/**
+ * Immutable deep merging objects.
+ */
+export function deepMerge(...data: any[]): any {
+  return [{}, ...data].reduce((result, current) => {
     Object.keys(current).forEach(key => {
       const resultValue = result[key];
       const currentValue = current[key];
 
       if (Array.isArray(resultValue) && Array.isArray(currentValue)) {
         result[key] = resultValue.concat(...currentValue);
-      } else if (isObject(resultValue) && isObject(currentValue)) {
-        result[key] = mergeDeep(resultValue, currentValue);
+      } else if (Array.isArray(currentValue)) {
+        result[key] = [...currentValue];
+      } else if (isPlainObject(resultValue) && isPlainObject(currentValue)) {
+        result[key] = deepMerge(resultValue, currentValue);
+      } else if (isPlainObject(currentValue)) {
+        result[key] = deepMerge({}, currentValue);
       } else {
         result[key] = currentValue;
-      }
+      };
     });
 
     return result;
   }, {});
 }
 
-function isObject(x: any): x is object {
-  return (x && typeof x === 'object' && !Array.isArray(x));
+
+/**
+ * Is this thing an object
+ */
+export function isObject(x: any): x is object {
+  return Boolean(x && typeof x === 'object' && !Array.isArray(x));
+}
+
+/**
+ * Is this thing a plain object (i.e. `{a: 1}`)
+ */
+export function isPlainObject(x: any): x is object {
+  return Boolean(isObject(x) && Object.getPrototypeOf(x) === Object.prototype);
 }
