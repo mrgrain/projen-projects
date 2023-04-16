e2b8c4fa65171341655d7c61e8965e6f32983210
Momo Kornher <mail@moritzkornher.de>
2023-04-16 16:41:06 +0100

    generate logo

--- src/logo/Logo.ts
+++ src/logo/Logo.ts
@@ -36,7 +36,50 @@ export interface LogoInfo extends LogoConfig {
  * Options to create a logo from an SVG string.
  */
 export interface LogoFromContentOptions extends LogoConfig {
-  readonly outfile?: string;
+  /**
+   * The path where the logo file will be created.
+   */
+  readonly outFile?: string;
+}
+
+/**
+ * Options to create a logo from an SVG string.
+ */
+export interface LogoForProjenOptions {
+  /**
+   * The path where the logo file will be created.
+   */
+  readonly outFile?: string;
+  /**
+   * The hex code for the color of the parcel outline.
+   * @default "#66200b"
+   */
+  readonly outlineColor?: string;
+  /**
+   * The hex code for the color of the packaging tape.
+   * @default "#cbdada"
+   */
+  readonly tapeColor?: string;
+  /**
+   * The hex code for the color of the front of the box.
+   * @default "#fa983c"
+   */
+  readonly frontColor?: string;
+  /**
+   * The hex code for the color of the top of the box.
+   * @default "#fcc161"
+   */
+  readonly topColor?: string;
+  /**
+   * The icon in front of the parcel box.
+   * @default - no icon
+   */
+  readonly icon?: string;
+  /**
+   * SVG transform for the icon.
+   * Likely needed to position the icon correctly.
+   */
+  readonly iconTransform?: string;
 }
 
 interface LogoFromOptions extends LogoConfig {
@@ -76,7 +119,6 @@ export class Logo implements ILogo {
     });
   }
 
-
   /**
    * An empty placeholder logo.
    * Can be used the get started before a logo is created.
@@ -90,6 +132,49 @@ export class Logo implements ILogo {
       outFile: path,
       width: 80,
       height: 80,
+    });
+  }
+
+  /**
+   * A logo for projen related projects.
+   */
+  public static forProjen(path: string = 'images/logo.svg', options: LogoForProjenOptions = {}) {
+    const {
+      topColor = '#fcc161',
+      frontColor = '#fa983c',
+      tapeColor = '#cbdada',
+      outlineColor = '#66200b',
+      icon,
+      iconTransform = '',
+    } = options;
+
+    const iconGroup = icon ? `<g transform="${iconTransform}">${icon}</g>` : '';
+
+    const content = `<g transform="translate(4 19)" shape-rendering="crispEdges">
+    <!-- fill -->
+    <path d="m 0,0 l 10,-15.6 h 60 l 10,15.6 z" fill="${topColor}" />
+    <path d="m 0,0 h 80 v 65 h -80 z" fill="${frontColor}" />
+
+    <!-- lines -->
+    <g stroke-width="2.5" stroke="${outlineColor}">
+        <!-- outline -->
+        <path d="m 0,0 l 10,-15.6 h 60 l 10,15.6 v 65 h -80 z" fill-opacity="0" />
+
+        <!-- tape -->
+        <path d="m 30,0 v 12 l 5,-3 5,3 5,-3 5,3 v -12 l -4.5,-15.6 -11,0 z" fill="${tapeColor}" stroke-linejoin="bevel" />
+
+        <!-- front line -->
+        <line x1="0" x2="80" y1="0" y2="0" />
+    </g>
+
+    <!-- Custom Icon -->
+    ${iconGroup}
+</g>`;
+
+    return new Logo(content, {
+      outFile: path,
+      width: 88,
+      height: 88,
     });
   }
 
