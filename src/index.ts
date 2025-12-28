a387c75e0131a3d25b950ae437c88ab41749cfc5
Momo Kornher <mail@moritzkornher.de>
2025-12-28 13:28:00 +0100

    feat: publish features (#169)
    Publish features so they can be re-used in other projects.

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,7 @@ export * from './projen-project-options';
 export * from './typescript-project';
 export * from './typescript-project-options';
 export * as utils from './utils';
+export { configureFeatures } from './utils';
 export * as logo from './logo';
 export * as components from './components';
+export * as features from './features';
